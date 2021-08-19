c061541d3af3e90de2621432f86e451c427f0a95
0522chan <0522chan@naver.com>
2021-08-19 15:05:02 +0900

    f

---
+++ TraceUI/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TraceUI.Model
+{
+    class Model
+    {
+        public double TimeStamp { get; set; }
+        public string StartOfFrame { get; set; }
+        public string Chn { get; set; }
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Dr { get; set; }
+        public int DLC { get; set; }
+        public byte Data { get; set; }
+    }
+}
---
+++ TraceUI/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Xpf.Core;
+using System.Collections.ObjectModel;
+using System.Windows;
+using TraceUI.Model;
+
+namespace TraceUI.ViewModel
+{
+    class ViewModel : ViewModelBase
+    {
+        Model.Model m = null;
+
+        public ViewModel()
+        {
+            m = new Model.Model();
+            AddData();
+        }
+
+        ObservableCollection<Model.Model> _sampleDatas = null;
+        public ObservableCollection<Model.Model> SampleDatas
+        {
+            get
+            {
+                if (_sampleDatas == null)
+                {
+                    _sampleDatas = new ObservableCollection<Model.Model>();
+                }
+                return _sampleDatas;
+            }
+            private set
+            {          
+                _sampleDatas = value;
+            }
+
+        }
+        public void AddData()
+        {
+            Model.Model m = new Model.Model();
+            m.TimeStamp = 3774.405045;
+            m.StartOfFrame = null;
+            m.Chn = null;
+            m.ID = "1CF";
+            m.Name = null;
+            m.Dr = "NONE";
+            m.DLC = 8;
+            m.Data = 00000000000000;
+
+            SampleDatas.Add(m);
+        }
+        
+
+
+    }
+
+}
